f8c0209e8a80b74d7ce705b9fdea307c58236a0a
Aberham <aberham@localhost.localdomain>
2024-01-05 13:26:18 -0700

    first pyton code

---
+++ hello.py
@@ -0,0 +1,16 @@
+temprature = eval(input("enter number"))
+if temprature > 30:
+    print("it is a hot day")
+    print("please wear a nice t-shert")
+elif temprature > 30:
+    print ("normal day")
+elif temprature >20:
+    print('it is a nice day')
+elif temprature > 10:
+    print("it is bit cold")
+    print("wear a hot clothes ")
+elif temprature > -20:
+    print("it is cold")
+else:
+    print("warming")
+print("thanks")
