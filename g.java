73f0a0e73a20367c40a4da75d760ffc8e2f14805
aberham3 <155385068+aberham3@users.noreply.github.com>
2024-01-15 03:33:40 -0700

    day two

---
+++ g.java
@@ -0,0 +1,13 @@
+public class g {
+    public static void selctionsor(int[]arr){
+        int n=arr.length;
+        for(int i =0; i<n-1; i++){
+        int midindex= i;
+        for (int x) {
+            
+        }
+
+    }
+
+    
+}
